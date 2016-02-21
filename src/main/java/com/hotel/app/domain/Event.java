277370b086665e1aa21f52cc007ce722ffa446f8
le huu dien <le huu dien>
2016-02-22 01:18:12 +0700

    create su kien va trang thai

---
+++ src/main/java/com/hotel/app/domain/Event.java
@@ -0,0 +1,165 @@
+package com.hotel.app.domain;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Objects;
+
+/**
+ * A Event.
+ */
+@Entity
+@Table(name = "event")
+public class Event implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Size(max = 255)
+    @Column(name = "title", length = 255)
+    private String title;
+
+    @Size(max = 255)
+    @Column(name = "decription", length = 255)
+    private String decription;
+
+    @Column(name = "start_date")
+    private LocalDate start_date;
+
+    @Column(name = "end_date")
+    private LocalDate end_date;
+
+    @Column(name = "create_date")
+    private ZonedDateTime create_date;
+
+    @Column(name = "last_modified_date")
+    private ZonedDateTime last_modified_date;
+
+    @ManyToOne
+    @JoinColumn(name = "status_pe_id")
+    private Status_pe status_pe;
+
+    @ManyToOne
+    @JoinColumn(name = "create_by_id")
+    private User create_by;
+
+    @ManyToOne
+    @JoinColumn(name = "last_modified_by_id")
+    private User last_modified_by;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+
+    public LocalDate getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(LocalDate start_date) {
+        this.start_date = start_date;
+    }
+
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(LocalDate end_date) {
+        this.end_date = end_date;
+    }
+
+    public ZonedDateTime getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(ZonedDateTime create_date) {
+        this.create_date = create_date;
+    }
+
+    public ZonedDateTime getLast_modified_date() {
+        return last_modified_date;
+    }
+
+    public void setLast_modified_date(ZonedDateTime last_modified_date) {
+        this.last_modified_date = last_modified_date;
+    }
+
+    public Status_pe getStatus_pe() {
+        return status_pe;
+    }
+
+    public void setStatus_pe(Status_pe status_pe) {
+        this.status_pe = status_pe;
+    }
+
+    public User getCreate_by() {
+        return create_by;
+    }
+
+    public void setCreate_by(User user) {
+        this.create_by = user;
+    }
+
+    public User getLast_modified_by() {
+        return last_modified_by;
+    }
+
+    public void setLast_modified_by(User user) {
+        this.last_modified_by = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Event event = (Event) o;
+        return Objects.equals(id, event.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+            "id=" + id +
+            ", title='" + title + "'" +
+            ", decription='" + decription + "'" +
+            ", start_date='" + start_date + "'" +
+            ", end_date='" + end_date + "'" +
+            ", create_date='" + create_date + "'" +
+            ", last_modified_date='" + last_modified_date + "'" +
+            '}';
+    }
+}
---
+++ src/test/java/com/hotel/app/web/rest/EventResourceIntTest.java
@@ -0,0 +1,228 @@
+package com.hotel.app.web.rest;
+
+import com.hotel.app.Application;
+import com.hotel.app.domain.Event;
+import com.hotel.app.repository.EventRepository;
+import com.hotel.app.service.EventService;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.hamcrest.Matchers.hasItem;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.time.LocalDate;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.ZoneId;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+/**
+ * Test class for the EventResource REST controller.
+ *
+ * @see EventResource
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest
+public class EventResourceIntTest {
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(ZoneId.of("Z"));
+
+    private static final String DEFAULT_TITLE = "AAAAA";
+    private static final String UPDATED_TITLE = "BBBBB";
+    private static final String DEFAULT_DECRIPTION = "AAAAA";
+    private static final String UPDATED_DECRIPTION = "BBBBB";
+
+    private static final LocalDate DEFAULT_START_DATE = LocalDate.ofEpochDay(0L);
+    private static final LocalDate UPDATED_START_DATE = LocalDate.now(ZoneId.systemDefault());
+
+    private static final LocalDate DEFAULT_END_DATE = LocalDate.ofEpochDay(0L);
+    private static final LocalDate UPDATED_END_DATE = LocalDate.now(ZoneId.systemDefault());
+
+    private static final ZonedDateTime DEFAULT_CREATE_DATE = ZonedDateTime.ofInstant(Instant.ofEpochMilli(0L), ZoneId.systemDefault());
+    private static final ZonedDateTime UPDATED_CREATE_DATE = ZonedDateTime.now(ZoneId.systemDefault()).withNano(0);
+    private static final String DEFAULT_CREATE_DATE_STR = dateTimeFormatter.format(DEFAULT_CREATE_DATE);
+
+    private static final ZonedDateTime DEFAULT_LAST_MODIFIED_DATE = ZonedDateTime.ofInstant(Instant.ofEpochMilli(0L), ZoneId.systemDefault());
+    private static final ZonedDateTime UPDATED_LAST_MODIFIED_DATE = ZonedDateTime.now(ZoneId.systemDefault()).withNano(0);
+    private static final String DEFAULT_LAST_MODIFIED_DATE_STR = dateTimeFormatter.format(DEFAULT_LAST_MODIFIED_DATE);
+
+    @Inject
+    private EventRepository eventRepository;
+
+    @Inject
+    private EventService eventService;
+
+    @Inject
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Inject
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    private MockMvc restEventMockMvc;
+
+    private Event event;
+
+    @PostConstruct
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        EventResource eventResource = new EventResource();
+        ReflectionTestUtils.setField(eventResource, "eventService", eventService);
+        this.restEventMockMvc = MockMvcBuilders.standaloneSetup(eventResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setMessageConverters(jacksonMessageConverter).build();
+    }
+
+    @Before
+    public void initTest() {
+        event = new Event();
+        event.setTitle(DEFAULT_TITLE);
+        event.setDecription(DEFAULT_DECRIPTION);
+        event.setStart_date(DEFAULT_START_DATE);
+        event.setEnd_date(DEFAULT_END_DATE);
+        event.setCreate_date(DEFAULT_CREATE_DATE);
+        event.setLast_modified_date(DEFAULT_LAST_MODIFIED_DATE);
+    }
+
+    @Test
+    @Transactional
+    public void createEvent() throws Exception {
+        int databaseSizeBeforeCreate = eventRepository.findAll().size();
+
+        // Create the Event
+
+        restEventMockMvc.perform(post("/api/events")
+                .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                .content(TestUtil.convertObjectToJsonBytes(event)))
+                .andExpect(status().isCreated());
+
+        // Validate the Event in the database
+        List<Event> events = eventRepository.findAll();
+        assertThat(events).hasSize(databaseSizeBeforeCreate + 1);
+        Event testEvent = events.get(events.size() - 1);
+        assertThat(testEvent.getTitle()).isEqualTo(DEFAULT_TITLE);
+        assertThat(testEvent.getDecription()).isEqualTo(DEFAULT_DECRIPTION);
+        assertThat(testEvent.getStart_date()).isEqualTo(DEFAULT_START_DATE);
+        assertThat(testEvent.getEnd_date()).isEqualTo(DEFAULT_END_DATE);
+        assertThat(testEvent.getCreate_date()).isEqualTo(DEFAULT_CREATE_DATE);
+        assertThat(testEvent.getLast_modified_date()).isEqualTo(DEFAULT_LAST_MODIFIED_DATE);
+    }
+
+    @Test
+    @Transactional
+    public void getAllEvents() throws Exception {
+        // Initialize the database
+        eventRepository.saveAndFlush(event);
+
+        // Get all the events
+        restEventMockMvc.perform(get("/api/events?sort=id,desc"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.[*].id").value(hasItem(event.getId().intValue())))
+                .andExpect(jsonPath("$.[*].title").value(hasItem(DEFAULT_TITLE.toString())))
+                .andExpect(jsonPath("$.[*].decription").value(hasItem(DEFAULT_DECRIPTION.toString())))
+                .andExpect(jsonPath("$.[*].start_date").value(hasItem(DEFAULT_START_DATE.toString())))
+                .andExpect(jsonPath("$.[*].end_date").value(hasItem(DEFAULT_END_DATE.toString())))
+                .andExpect(jsonPath("$.[*].create_date").value(hasItem(DEFAULT_CREATE_DATE_STR)))
+                .andExpect(jsonPath("$.[*].last_modified_date").value(hasItem(DEFAULT_LAST_MODIFIED_DATE_STR)));
+    }
+
+    @Test
+    @Transactional
+    public void getEvent() throws Exception {
+        // Initialize the database
+        eventRepository.saveAndFlush(event);
+
+        // Get the event
+        restEventMockMvc.perform(get("/api/events/{id}", event.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.id").value(event.getId().intValue()))
+            .andExpect(jsonPath("$.title").value(DEFAULT_TITLE.toString()))
+            .andExpect(jsonPath("$.decription").value(DEFAULT_DECRIPTION.toString()))
+            .andExpect(jsonPath("$.start_date").value(DEFAULT_START_DATE.toString()))
+            .andExpect(jsonPath("$.end_date").value(DEFAULT_END_DATE.toString()))
+            .andExpect(jsonPath("$.create_date").value(DEFAULT_CREATE_DATE_STR))
+            .andExpect(jsonPath("$.last_modified_date").value(DEFAULT_LAST_MODIFIED_DATE_STR));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingEvent() throws Exception {
+        // Get the event
+        restEventMockMvc.perform(get("/api/events/{id}", Long.MAX_VALUE))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updateEvent() throws Exception {
+        // Initialize the database
+        eventRepository.saveAndFlush(event);
+
+		int databaseSizeBeforeUpdate = eventRepository.findAll().size();
+
+        // Update the event
+        event.setTitle(UPDATED_TITLE);
+        event.setDecription(UPDATED_DECRIPTION);
+        event.setStart_date(UPDATED_START_DATE);
+        event.setEnd_date(UPDATED_END_DATE);
+        event.setCreate_date(UPDATED_CREATE_DATE);
+        event.setLast_modified_date(UPDATED_LAST_MODIFIED_DATE);
+
+        restEventMockMvc.perform(put("/api/events")
+                .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                .content(TestUtil.convertObjectToJsonBytes(event)))
+                .andExpect(status().isOk());
+
+        // Validate the Event in the database
+        List<Event> events = eventRepository.findAll();
+        assertThat(events).hasSize(databaseSizeBeforeUpdate);
+        Event testEvent = events.get(events.size() - 1);
+        assertThat(testEvent.getTitle()).isEqualTo(UPDATED_TITLE);
+        assertThat(testEvent.getDecription()).isEqualTo(UPDATED_DECRIPTION);
+        assertThat(testEvent.getStart_date()).isEqualTo(UPDATED_START_DATE);
+        assertThat(testEvent.getEnd_date()).isEqualTo(UPDATED_END_DATE);
+        assertThat(testEvent.getCreate_date()).isEqualTo(UPDATED_CREATE_DATE);
+        assertThat(testEvent.getLast_modified_date()).isEqualTo(UPDATED_LAST_MODIFIED_DATE);
+    }
+
+    @Test
+    @Transactional
+    public void deleteEvent() throws Exception {
+        // Initialize the database
+        eventRepository.saveAndFlush(event);
+
+		int databaseSizeBeforeDelete = eventRepository.findAll().size();
+
+        // Get the event
+        restEventMockMvc.perform(delete("/api/events/{id}", event.getId())
+                .accept(TestUtil.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
+
+        // Validate the database is empty
+        List<Event> events = eventRepository.findAll();
+        assertThat(events).hasSize(databaseSizeBeforeDelete - 1);
+    }
+}
