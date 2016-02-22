2be644e8cab0b1f68f7027c0d32ed2ad62fb6656
le huu dien <le huu dien>
2016-02-22 21:55:03 +0700

    them trang thai dat phong

---
+++ src/main/java/com/hotel/app/domain/Status_reservation.java
@@ -0,0 +1,105 @@
+package com.hotel.app.domain;
+
+import java.time.ZonedDateTime;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Objects;
+
+/**
+ * A Status_reservation.
+ */
+@Entity
+@Table(name = "status_reservation")
+public class Status_reservation implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "name", length = 255, nullable = false)
+    private String name;
+
+    @Size(max = 255)
+    @Column(name = "decription", length = 255)
+    private String decription;
+
+    @Column(name = "create_date")
+    private ZonedDateTime create_date;
+
+    @ManyToOne
+    @JoinColumn(name = "create_by_id")
+    private User create_by;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public void setDecription(String decription) {
+        this.decription = decription;
+    }
+
+    public ZonedDateTime getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(ZonedDateTime create_date) {
+        this.create_date = create_date;
+    }
+
+    public User getCreate_by() {
+        return create_by;
+    }
+
+    public void setCreate_by(User user) {
+        this.create_by = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Status_reservation status_reservation = (Status_reservation) o;
+        return Objects.equals(id, status_reservation.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Status_reservation{" +
+            "id=" + id +
+            ", name='" + name + "'" +
+            ", decription='" + decription + "'" +
+            ", create_date='" + create_date + "'" +
+            '}';
+    }
+}
---
+++ src/main/java/com/hotel/app/service/impl/Status_reservationServiceImpl.java
@@ -0,0 +1,68 @@
+package com.hotel.app.service.impl;
+
+import com.hotel.app.service.Status_reservationService;
+import com.hotel.app.domain.Status_reservation;
+import com.hotel.app.repository.Status_reservationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Service Implementation for managing Status_reservation.
+ */
+@Service
+@Transactional
+public class Status_reservationServiceImpl implements Status_reservationService{
+
+    private final Logger log = LoggerFactory.getLogger(Status_reservationServiceImpl.class);
+    
+    @Inject
+    private Status_reservationRepository status_reservationRepository;
+    
+    /**
+     * Save a status_reservation.
+     * @return the persisted entity
+     */
+    public Status_reservation save(Status_reservation status_reservation) {
+        log.debug("Request to save Status_reservation : {}", status_reservation);
+        Status_reservation result = status_reservationRepository.save(status_reservation);
+        return result;
+    }
+
+    /**
+     *  get all the status_reservations.
+     *  @return the list of entities
+     */
+    @Transactional(readOnly = true) 
+    public Page<Status_reservation> findAll(Pageable pageable) {
+        log.debug("Request to get all Status_reservations");
+        Page<Status_reservation> result = status_reservationRepository.findAll(pageable); 
+        return result;
+    }
+
+    /**
+     *  get one status_reservation by id.
+     *  @return the entity
+     */
+    @Transactional(readOnly = true) 
+    public Status_reservation findOne(Long id) {
+        log.debug("Request to get Status_reservation : {}", id);
+        Status_reservation status_reservation = status_reservationRepository.findOne(id);
+        return status_reservation;
+    }
+
+    /**
+     *  delete the  status_reservation by id.
+     */
+    public void delete(Long id) {
+        log.debug("Request to delete Status_reservation : {}", id);
+        status_reservationRepository.delete(id);
+    }
+}
